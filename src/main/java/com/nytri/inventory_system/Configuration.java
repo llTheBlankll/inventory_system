1fe9969679432cbc4bada2ba19c892cb06878c82
Nytri <llTheBlankll@gmail.com>
2022-06-20 19:15:27 +0800

    Pagination little enhancement.

--- src/main/java/com/nytri/inventory_system/Configuration.java
+++ src/main/java/com/nytri/inventory_system/Configuration.java
@@ -3,5 +3,7 @@ package com.nytri.inventory_system;
 public class Configuration {
 
     public static final int low_stock_items_max_row = 10;
-    public static final int stock_pagination_max_row = 10;
+    public static final int stock_pagination_max_data = 10;
+    public static final int categories_pagination_max_data = 10;
+    public static final int suppliers_pagination_max_data = 10;
 }
--- src/main/java/com/nytri/inventory_system/controller/Paginations.java
+++ src/main/java/com/nytri/inventory_system/controller/Paginations.java
@@ -1,7 +1,9 @@
 package com.nytri.inventory_system.controller;
 
 import com.nytri.inventory_system.Configuration;
+import com.nytri.inventory_system.repositories.CategoriesRepository;
 import com.nytri.inventory_system.repositories.StockRepository;
+import com.nytri.inventory_system.repositories.SupplierRepository;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,32 +15,86 @@ import java.util.List;
 @RequestMapping("/api/pagination/numbering")
 public class Paginations {
 
-    private final StockRepository stockRepository;
+  private final StockRepository stockRepository;
+  private final CategoriesRepository categoriesRepository;
+  private final SupplierRepository supplierRepository;
 
-    public Paginations(StockRepository stockRepository) {
-        this.stockRepository = stockRepository;
+  public Paginations(StockRepository stockRepository,
+                     SupplierRepository supplierRepository,
+                     CategoriesRepository categoriesRepository) {
+    this.categoriesRepository = categoriesRepository;
+    this.supplierRepository = supplierRepository;
+    this.stockRepository = stockRepository;
+  }
+
+  @GetMapping("/stocks/{page}")
+  public List<Integer> getStocksPaginationNumbersIfInPage(@PathVariable("page") Integer pageNum) {
+    List<Integer> pagination = new ArrayList<>();
+    if (pageNum - 1 >= 0) {
+      if (pageNum - 2 >= 0) {
+        pagination.add(pageNum - 2);
+      }
+      pagination.add(pageNum - 1);
     }
 
-    @GetMapping("/stocks/{page}")
-    public List<Integer> getPaginationNumbersIfInPage(@PathVariable("page") Integer pageNum) {
-        Integer total_rows = stockRepository.getStocksTotalCount();
-        List<Integer> pagination = new ArrayList<>();
-        if (pageNum - 1 >= 0) {
-            if (pageNum - 2 >= 0) {
-                pagination.add(pageNum - 2);
-            }
-            pagination.add(pageNum - 1);
-        }
+    pagination.add(pageNum);
+    if (stockRepository.findAll(PageRequest.of(pageNum + 1, Configuration.stock_pagination_max_data)).hasContent()) {
+      if (stockRepository.findAll(PageRequest.of(pageNum + 2, Configuration.stock_pagination_max_data)).hasContent()) {
+        pagination.add(pageNum + 2);
+      }
 
-        pagination.add(pageNum);
-        if (stockRepository.findAll(PageRequest.of(pageNum + 1, Configuration.stock_pagination_max_row)).hasContent()) {
-            pagination.add(pageNum + 1);
-        }
-
-        if (stockRepository.findAll(PageRequest.of(pageNum + 2, Configuration.stock_pagination_max_row)).hasContent()) {
-            pagination.add(pageNum + 2);
-        }
-
-        return pagination;
+      pagination.add(pageNum + 1);
     }
+
+    return pagination;
+  }
+
+  @GetMapping("/categories/{page}")
+  public List<Integer> getCategoriesPaginationNumbersIfInPage(@PathVariable("page") Integer pageNum) {
+    List<Integer> pagination = new ArrayList<>();
+
+    if (pageNum - 1 >= 0) {
+      if (pageNum - 2 >= 0) {
+        pagination.add(pageNum - 2);
+      }
+      pagination.add(pageNum - 1);
+    }
+
+    pagination.add(pageNum);
+
+    if (categoriesRepository.findAll(PageRequest.of(pageNum + 1, Configuration.categories_pagination_max_data)).hasContent()) {
+      if (categoriesRepository.findAll(PageRequest.of(pageNum + 2, Configuration.stock_pagination_max_data)).hasContent()) {
+        pagination.add(pageNum + 2);
+      }
+
+      pagination.add(pageNum + 1);
+    }
+
+    return pagination;
+  }
+
+  @GetMapping("/suppliers/{page}")
+  public List<Integer> getSuppliersPaginationNumbersIfInPage(@PathVariable("page") Integer pageNum) {
+    List<Integer> pagination = new ArrayList<>();
+
+    if (pageNum - 1 >= 0) {
+      if (pageNum - 2 >= 0) {
+        pagination.add(pageNum + 2);
+      }
+
+      pagination.add(pageNum + 1);
+    }
+
+    pagination.add(pageNum);
+
+    if (supplierRepository.findAll(PageRequest.of(pageNum + 1, Configuration.suppliers_pagination_max_data)).hasContent()) {
+      if (supplierRepository.findAll(PageRequest.of(pageNum + 2, Configuration.suppliers_pagination_max_data)).hasContent()) {
+        pagination.add(pageNum + 2);
+      }
+
+      pagination.add(pageNum + 1);
+    }
+
+    return pagination;
+  }
 }
